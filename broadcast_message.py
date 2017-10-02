17ea58bb59b1464276ca2b348dfbcf118474754f
peter <piter.zh@gmail.com>
2017-10-03 00:24:03 +0300

    Changed broadcast message

--- broadcast_message.py
+++ broadcast_message.py
@@ -4,15 +4,7 @@ from telegram.error import BadRequest, Unauthorized
 import access_token
 from database import Databases
 
-broadcast = "Я тут добавил поиск свободных аудиторий. Пока это работает только для тех корпусов, " \
-            "где в этот день у вас проходят занятия. " \
-            "Для других корпусов не очень понятно, что делать (так как у ВШЭ для РУЗа более 50 корпусов), но," \
-            " скорее всего, потом " \
-            "сделаю выбор из тех корпусов, в которых у вас не так давно были пары.\n" \
-            "И да, в некоторых зданиях аудитории имеют достаточно наркоманские имена. Если " \
-            "увидите, то присылайте мне сообщения с такими аудиториями в телеграм @PeterZhizhin," \
-            " мы подумаем, что с этим можно сделать. Также пишите, если просто хотите поболтать, можем " \
-            "обсудить то, чего боту не хватает.\n"
+broadcast = """Мне сообщают о проблемах с ботом. К сожалению, по какой-то причине, на любой запрос РУЗ отвечает 404 (страница не найдена). А при попытке открыть ruz.hse.ru - ничего не получется, страница висит."""
 no_mail = "Вы не указали почту, попробуйте сделать это сейчас, ведь интерфейс теперь и правда понятный!\nСпасибо."
 
 broadcast_with_mail = broadcast
@@ -31,13 +23,9 @@ def main():
         if user_id in already_sent_to:
             continue
         try:
-            if 'email' not in user or user['email'] is None:
-                bot.send_message(user_id, broadcast_no_mail)
-                print(user_id)
-            else:
-                email = user['email']
-                bot.send_message(user_id, broadcast_with_mail.format(email))
-                print(user_id)
+            email = user['email']
+            bot.send_message(user_id, broadcast_with_mail.format(email))
+            print(user_id)
         except BadRequest:
             wrong_count += 1
             print(user_id, 'bad request')
